055f1c6ab5732d4b6f73e677daa160b61be81912
Funsy <egorabramovich2012@mail.ru>
2026-06-19 21:20:44 +0300

    feat: add page switch

---
+++ frontend/js/script.js
@@ -0,0 +1,31 @@
+const buttons = document.querySelectorAll(
+    ".panel-button, .proxy-settings-button, .logs-button, .panel-settings-button"
+);
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        buttons.forEach(btn => btn.classList.remove("active"));
+        button.classList.add("active");
+
+        const page = button.dataset.page;
+        const page_iframe = document.querySelector(".page-iframe");
+
+        switch (page) {
+            case "panel":
+                page_iframe.src = "panel.html";
+                break;
+
+            case "proxy-settings":
+                page_iframe.src = "proxy_settings.html";
+                break;
+
+            case "logs":
+                page_iframe.src = "logs.html";
+                break;
+
+            case "panel-settings":
+                page_iframe.src = "panel_settings.html";
+                break;
+        }
+    });
+});
