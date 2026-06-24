333380cf7b852c1847792afef8cc379162c1023a
i4ego <me@i4ego.ru>
2026-06-24 16:07:04 +0500

    feat: add page title changing logic

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -2,6 +2,8 @@ const buttons = document.querySelectorAll(
     ".panel-button, .proxy-settings-button, .logs-button, .panel-settings-button"
 );
 
+const pageTitle = document.getElementById("page-title")
+
 buttons.forEach(button => {
     button.addEventListener("click", () => {
         buttons.forEach(btn => btn.classList.remove("active"));
@@ -13,18 +15,22 @@ buttons.forEach(button => {
         switch (page) {
             case "panel":
                 page_iframe.src = "dashboard.html";
+                pageTitle.textContent = "GO-UI - Дэшборд"
                 break;
 
             case "proxy-settings":
                 page_iframe.src = "proxy_settings.html";
+                pageTitle.textContent = "GO-UI - Прокси"
                 break;
 
             case "logs":
                 page_iframe.src = "logs.html";
+                pageTitle.textContent = "GO-UI - Логи"
                 break;
 
             case "panel-settings":
                 page_iframe.src = "panel_settings.html";
+                pageTitle.textContent = "GO-UI - Настройки"
                 break;
         }
     });
